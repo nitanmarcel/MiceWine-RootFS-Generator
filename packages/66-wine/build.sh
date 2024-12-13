ec6c8adad600185307d21588662f40ecad2144ca
Marcel Alexandru Nitan <nitan.marcel@protonmail.com>
2024-12-13 19:41:20 +0200

    Use patched wine hangover

--- packages/66-wine/build.sh
+++ packages/66-wine/build.sh
@@ -4,8 +4,8 @@ PKG_PRETTY_NAME="Wine ($PKG_VER)"
 
 BLACKLIST_ARCHITECTURE=aarch64
 
-GIT_URL=https://github.com/KreitinnSoftware/wine
-GIT_COMMIT=3850cee2f69a568ed4c31f462c82b9e3b2f5b342
+GIT_URL=https://github.com/nitanmarcel/wine-hangover
+GIT_COMMIT=c9ee7ef185e8a0b848bf1a67ddaac29c5a8b319c
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
